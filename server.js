c5a9f78a1abf5ef03ece66806cdaf4d8c0c5ec5d
Lauri Peltonen <lpeltonen@nvidia.com>
2015-02-19 15:20:36 +0200

    Serve angular app through express

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var express       = require('express');
 var app = express();
 var port = process.env.PORT || 80;
 
+app.use('/src', express.static('src'));
+app.use('/bower_components', express.static('bower_components'));
+
+app.get('/', function (req, res) { res.sendFile(__dirname + '/index.html'); });
+
 app.get('/version', function(req, res) {
   res.json({
     version: 1.0,
